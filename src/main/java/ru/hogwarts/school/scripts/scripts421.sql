f5140986edb9690716ea2763e1e2b9718e757ea4
Fet2113 <super.gjoll@yandex.ru>
2023-10-25 11:31:57 +0300

    HW 3.8. Scripts На проверку.

---
+++ src/main/java/ru/hogwarts/school/scripts/scripts421.sql
@@ -0,0 +1,25 @@
+-- Возраст студента не может быть меньше 16 лет.
+
+alter table student(
+add constraints student_age_constraint check(age > 16));
+
+
+-- Имена студентов должны быть уникальными и не равны нулю.
+
+alter table student(
+add constraints student_name_constraint unique(name));
+
+alter table student
+column name set not null;
+
+
+-- Пара “значение названия” - “цвет факультета” должна быть уникальной.
+
+alter table faculty(
+add constraints faculty_name_and_color_constraint unique(name, color));
+
+
+-- При создании студента без возраста ему автоматически должно присваиваться 20 лет.
+
+alter table student
+column age set default 20;
---
+++ src/main/java/ru/hogwarts/school/scripts/scripts422.sql
@@ -0,0 +1,16 @@
+create table car (
+	id int primary key,
+	mark text ,
+	model text ,
+	price int
+);
+
+
+create table person (
+	id int primary key,
+	namee text,
+	agee int,
+	have_license bool,
+	car_id int,
+	foreign key (car_id) references car(id)
+);
